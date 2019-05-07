4acb5c366b32b972701c546d6c893c61ae9349a4
SlawomirJablonski <forest19@interia.pl>
2019-05-07 23:55:40 +0200

    mod ((( ex32.2

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,13 +19,16 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send(final Mail mail){
 
         LOGGER.info("Starting email preparation...");
 
         try{
-            SimpleMailMessage mailMassage = createMailMassage(mail);
-            //javaMailSender.send(mailMassage);
+            //SimpleMailMessage mailMassage = createMailMassage(mail);
+            javaMailSender.send(createMimeMessage(mail));
 
             LOGGER.info("Email has been sent");
 
@@ -33,7 +38,24 @@ public class SimpleEmailService {
 
     }
 
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
     private SimpleMailMessage createMailMassage(final Mail mail){
+        SimpleMailMessage mailMassage = new SimpleMailMessage();
+        mailMassage.setTo(mail.getMailTo());
+        mailMassage.setSubject(mail.getSubject());
+        mailMassage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        return mailMassage;
+    }
+
+    /*private SimpleMailMessage createMailMassage(final Mail mail){
         SimpleMailMessage mailMassage = new SimpleMailMessage();
         mailMassage.setTo(mail.getMailTo());
         mailMassage.setSubject(mail.getSubject());
@@ -42,5 +64,5 @@ public class SimpleEmailService {
             mailMassage.setCc(mail.getToCc());
         }
         return mailMassage;
-    }
+    }*/
 }
